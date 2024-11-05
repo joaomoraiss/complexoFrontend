58d69702ad6676e4dc235712f93fe6a6c4a509a4
joaomoraiss <joaovictorrmpework@outlook.com>
2024-11-05 12:47:14 -0300

    função hover em link

---
+++ src/utils/styles.js
@@ -0,0 +1,3 @@
+export const linkHoverEffect = () => `
+  relative after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[2px] after:bg-current after:transition-all after:duration-300 hover:after:w-full
+`;
