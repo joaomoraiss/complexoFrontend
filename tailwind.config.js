9b2e0274e789958d43500e6ae87fa50d685b4b33
joaomoraiss <joaovictorrmpework@outlook.com>
2024-11-12 11:44:05 -0300

    contato page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-color': '#B7B7B7',
+        'text-color' : '#6A6565',
+      },
+      screens: {
+        'phone': '375px',
+    },
   },
   plugins: [],
-};
+}}
